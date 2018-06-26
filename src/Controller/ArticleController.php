c7b84975d07bff3716eaa1da59f1bf235cf7a34b
annaS <dupa113@dupa.pl>
2018-06-26 17:50:43 +0200

    making so much good progress

---
+++ src/Controller/ArticleController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ArticleController
+{
+    /**
+     * @Route("/")
+     */
+    public function homepage()
+    {
+        return new Response('Omg! my first page. Wow!!');
+    }
+
+    /**
+     * @Route("/news/{slug}")
+     */
+    public function show($slug)
+    {
+        return new Response(sprintf(
+            'Future page to show the article: %s',
+            $slug
+        ));
+    }
+}
