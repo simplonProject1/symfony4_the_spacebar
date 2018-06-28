b8ad1d0cf49b316c4c2c23ba23a6bef8ee9be04f
annaS <dupa113@dupa.pl>
2018-06-28 16:22:05 +0200

    let's deploy

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -2,22 +2,23 @@
 
 namespace App\Controller;
 
+use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ArticleController extends AbstractController
 {
     /**
-     * @Route("/")
+     * @Route("/", name="app_homepage")
      */
     public function homepage()
     {
-        return new Response('Omg! my first page. Wow!!');
+        return $this->render('article/homepage.html.twig');
     }
 
     /**
-     * @Route("/news/{slug}")
+     * @Route("/news/{slug}", name="article_show")
      */
     public function show($slug)
     {
@@ -31,7 +32,19 @@ class ArticleController extends AbstractController
 
         return $this->render('article/show.html.twig', [
             'title' => ucwords(str_replace('-', ' ', $slug)),
+            'slug' => $slug,
             'comments' => $comments,
         ]);
+    }
+
+    /**
+     * @Route("/news/{slug}/heart", name="article_toggle_heart", methods={"POST"})
+     */
+    public function toggleArticleHeart ($slug)
+    {
+
+        // TODO - actually heart/unheart the article
+
+        return new JsonResponse(['hearts' => rand(5, 100)]);
     }
 }
