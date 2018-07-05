075d10a6c587441878631b4f8be21cc83ce0c07d
annaS <dupa113@dupa.pl>
2018-07-05 10:20:38 +0200

    cd

---
+++ src/Controller/ArticleAdminController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Article;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ArticleAdminController extends AbstractController
+{
+    /**
+     * @Route("/admin/article/new")
+     */
+    public function new(EntityManagerInterface $em)
+    {
+        $article = new Article();
+        $article->setTitle('No i po!!!')
+            ->setSlug('no-i-po-'.rand(100, 999))
+            ->setContent(<<<EOF
+Spicy **jalapeno bacon** ipsum dolor amet veniam shank in dolore. Ham hock nisi landjaeger cow,
+lorem proident [beef ribs](https://baconipsum.com/) aute enim veniam ut cillum pork chuck picanha. Dolore reprehenderit
+labore minim pork belly spare ribs cupim short loin in. Elit exercitation eiusmod dolore cow
+**turkey** shank eu pork belly meatball non cupim. mmmmmm
+
+Laboris beef ribs fatback fugiat eiusmod jowl kielbasa alcatra dolore velit ea ball tip. Pariatur
+laboris sunt venison, et laborum dolore minim non meatball. Shankle eu flank aliqua shoulder,
+capicola biltong frankfurter boudin cupim officia. Exercitation fugiat consectetur ham. Adipisicing
+picanha shank et filet mignon pork belly ut ullamco. Irure velit turducken ground round doner incididunt
+occaecat lorem meatball prosciutto quis strip steak.
+
+Meatball adipisicing ribeye bacon strip steak eu. Consectetur ham hock pork hamburger enim strip steak
+mollit quis officia meatloaf tri-tip swine. Cow ut reprehenderit, buffalo incididunt in filet mignon
+strip steak pork belly aliquip capicola officia. Labore deserunt esse chicken lorem shoulder tail consectetur
+cow est ribeye adipisicing. Pig hamburger pork belly enim. Do porchetta minim capicola irure pancetta chuck
+fugiat.
+EOF
+            );
+
+        // publish most articles
+        if (rand(1, 10) > 1) {
+            $article->setPublishedAt(new \DateTime(sprintf('-%d days', rand(1, 100))));
+        }
+
+        $article2 = new Article();
+        $article2->setTitle('Na szczęście zdążyłam!!!')
+            ->setSlug('na-szczescie-zdazylam-'.rand(100, 999))
+            ->setContent(<<<EOF
+Spicy **jalapeno bacon** ipsum dolor amet veniam shank in dolore. Ham hock nisi landjaeger cow,
+lorem proident [beef ribs](https://baconipsum.com/) aute enim veniam ut cillum pork chuck picanha. Dolore reprehenderit
+labore minim pork belly spare ribs cupim short loin in. Elit exercitation eiusmod dolore cow
+**turkey** shank eu pork belly meatball non cupim. yyyyyyyyyyyy
+
+Laboris beef ribs fatback fugiat eiusmod jowl kielbasa alcatra dolore velit ea ball tip. Pariatur
+laboris sunt venison, et laborum dolore minim non meatball. Shankle eu flank aliqua shoulder,
+capicola biltong frankfurter boudin cupim officia. Exercitation fugiat consectetur ham. Adipisicing
+picanha shank et filet mignon pork belly ut ullamco. Irure velit turducken ground round doner incididunt
+occaecat lorem meatball prosciutto quis strip steak.
+
+Meatball adipisicing ribeye bacon strip steak eu. Consectetur ham hock pork hamburger enim strip steak
+mollit quis officia meatloaf tri-tip swine. Cow ut reprehenderit, buffalo incididunt in filet mignon
+strip steak pork belly aliquip capicola officia. Labore deserunt esse chicken lorem shoulder tail consectetur
+cow est ribeye adipisicing. Pig hamburger pork belly enim. Do porchetta minim capicola irure pancetta chuck
+fugiat.
+EOF
+            );
+
+        // publish most articles
+        if (rand(1, 10) > 1) {
+            $article2->setPublishedAt(new \DateTime(sprintf('-%d days', rand(1, 100))));
+        }
+
+        $em->persist($article);
+        $em->persist($article2);
+        $em->flush();
+
+        return new Response(sprintf(
+            'Hiya! New Article id: #%d slug: %s. Second: id: #%d slug: %s',
+            $article->getId(),
+            $article->getSlug(),
+            $article2->getId(),
+            $article2->getSlug()
+        ));
+    }
+}
--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -2,10 +2,14 @@
 
 namespace App\Controller;
 
+use Michelf\MarkdownInterface;
 use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Cache\Adapter\AdapterInterface;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+// use Twig\Environment;
 
 class ArticleController extends AbstractController
 {
@@ -20,7 +24,7 @@ class ArticleController extends AbstractController
     /**
      * @Route("/news/{slug}", name="article_show")
      */
-    public function show($slug)
+    public function show($slug, MarkdownInterface $markdown, AdapterInterface $cache)
     {
         $comments = [
             'I ate a normal rock once. It did NOT taste like bacon!',
@@ -28,20 +32,60 @@ class ArticleController extends AbstractController
             'I like bacon too! Buy some from my site! bakinsomebacon.com',
         ];
 
-//        dump($slug, $this);
+        //  dump($slug, $this);
 
-        return $this->render('article/show.html.twig', [
+        $articleContent = <<<EOF
+Spicy **jalapeno bacon** ipsum dolor amet veniam shank in dolore. Ham hock nisi landjaeger cow, lorem proident [beef ribs](https://baconipsum.com/) aute enim veniam ut cillum pork chuck picanha. Dolore reprehenderit labore minim pork belly spare ribs cupim short loin in. Elit exercitation eiusmod dolore cow turkey shank eu pork belly meatball non cupim.
+
+Laboris **beef** ribs fatback fugiat eiusmod jowl kielbasa alcatra dolore velit ea ball tip. Pariatur laboris sunt venison, et laborum dolore minim non meatball. Shankle eu flank aliqua shoulder, capicola biltong frankfurter boudin cupim officia. Exercitation fugiat consectetur ham. Adipisicing picanha shank et filet mignon pork belly ut ullamco. Irure velit turducken ground round doner incididunt occaecat lorem meatball prosciutto quis strip steak.
+
+Meatball adipisicing ribeye bacon strip steak eu. Consectetur ham hock pork hamburger enim strip steak mollit quis officia meatloaf tri-tip swine. Cow ut reprehenderit, buffalo incididunt in filet mignon strip steak pork belly aliquip capicola officia. Labore deserunt esse chicken lorem shoulder tail consectetur cow est ribeye adipisicing. Pig hamburger pork belly enim. Do porchetta minim capicola irure pancetta chuck fugiat.
+
+EOF;
+        // $articleContent = $markdown->transform($articleContent);
+
+        $item = $cache->getItem('markdown_'.md5($articleContent));
+        if (!$item->isHit()) {
+            $item->set($articleContent = $markdown->transform($articleContent));
+            $cache->save($item);
+        }
+        $articleContent = $item->get();
+
+        // dump($markdown);die;
+
+        return $this->render('article/show.html.twig', [  // tutaj $this to twig object, ale skąd wiadomo?
             'title' => ucwords(str_replace('-', ' ', $slug)),
+            'articleContent' => $articleContent,
             'slug' => $slug,
             'comments' => $comments,
         ]);
     }
 
+//    public function show($slug, Environment $twigEnvironment)
+//    {
+//        $comments = [
+//            'I ate a normal rock once. It did NOT taste like bacon!',
+//            'Woohoo! I\'m going on an all-asteroid diet!',
+//            'I like bacon too! Buy some from my site! bakinsomebacon.com',
+//        ];
+//
+////        dump($slug, $this);
+//
+//        $html = $twigEnvironment->render('article/show.html.twig', [
+//            'title' => ucwords(str_replace('-', ' ', $slug)),
+//            'slug' => $slug,
+//            'comments' => $comments,
+//        ]);
+//
+//        return new Response($html);
+//    }
+
     /**
      * @Route("/news/{slug}/heart", name="article_toggle_heart", methods={"POST"})
      */
-    public function toggleArticleHeart ($slug)
+    public function toggleArticleHeart ($slug, LoggerInterface $logger)
     {
+        $logger->info('Article is being hearted');
 
         // TODO - actually heart/unheart the article
 
